0b34c579b2b7e8eff1dd776b133dc0b36bdb3595
Marco De Clara <marcodeclara@gmail.com>
2020-04-21 17:44:13 +0200

    Small adjustments

--- dadi/main.js
+++ dadi/main.js
@@ -1,19 +1,19 @@
 // "Gioco dei dadi" PC vs. Human: chiedi un numero all'utente tra 1 e 6 (giocata human) e estrai un numero casuale tra 1 e 6 (giocata PC). Chi fa il numero più alto vince!
 
 // ask player for a number between 1 and 6
-var playerNumber = prompt("Please enter a number between 1 and 6");
+var playerNumber = prompt( "Please enter a number between 1 and 6" );
 
 // get a random number in range between 1 and 6
-var cpuNumber = ( ( Math.floor( Math.random() * 10 ) % 6 ) + 1 );
+var cpuNumber = ( Math.floor( Math.random() * 6 ) + 1 );
 
 // compare playerNumber and cpuNumber
 // if playerNumber is greater than cpuNumber, the player wins
 if ( playerNumber > cpuNumber ) {
-    document.writeln("Player: " + playerNumber + ", Cpu: " + cpuNumber + ". You win!");
+    console.log( "Player: " + playerNumber + ", Cpu: " + cpuNumber + ". You win!" );
     // if playerNumber is lower than cpuNumber, cpu wins
 } else if ( playerNumber < cpuNumber ) {
-    document.writeln("Player: " + playerNumber + ", Cpu: " + cpuNumber + ". You lose!");
+    console.log( "Player: " + playerNumber + ", Cpu: " + cpuNumber + ". You lose!" );
     // if playerNumber is equal to cpuNumber, it's a tie
 } else {
-    document.writeln("Player: " + playerNumber + ", Cpu: " + cpuNumber + ". It's a tie!");
+    console.log( "Player: " + playerNumber + ", Cpu: " + cpuNumber + ". It's a tie!" );
 }
--- mail/main.js
+++ mail/main.js
@@ -1,17 +1,17 @@
 // Chiedi all'utente la sua email controlla che sia nella lista di chi può accedere (ergo: la mail è presente in un array contenente le email "autorizzate") e stampa un messaggio appropriato (sei in lista/non sei in lista)
 
 // array with authorized email addresses
-var list = ["pinco.pallino@gmail.com", "johndoe@hotmail.it", "jane.doe@yahoo.it", "nome.cognome@gmail.com", "cognomenomenumero@gmail.com"];
+var list = [ "pinco.pallino@gmail.com", "johndoe@hotmail.it", "jane.doe@yahoo.it", "nome.cognome@gmail.com", "cognomenomenumero@gmail.com" ];
 
 // initialize email counter
 var j = 0;
 
 // ask user their email address
-var email = prompt("Please enter your email address");
+var email = prompt( "Please enter your email address" );
 
 // for loop checks if the given email is in the list, j increases when a match is found
 for ( var i = 0; i < list.length; i++ ) {
-    if ( isRegistered( email, list[i] )) {
+    if ( isRegistered( email, list[i] ) ) {
         j++;
         break;
     }
@@ -19,13 +19,13 @@ for ( var i = 0; i < list.length; i++ ) {
 
 // if j > 0 the email given is in the list
 if ( j > 0 ) {
-    document.writeln("You have entered a registered email address.");
+    console.log( "You have entered a registered email address." );
 // else the email given is not the list
 } else {
-    document.writeln("Your email address was not found.");
+    console.log( "Your email address was not found." );
 }
 
 // function that compares two strings
-function isRegistered (a, b) {
+function isRegistered( a, b ) {
     return a == b;
 }
