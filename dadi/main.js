84c5eb710e9bd64679130d6cc891f44c5a2ec5d3
Marco De Clara <marcodeclara@gmail.com>
2020-04-21 14:52:09 +0200

    "Gioco dei dadi" done

--- dadi/main.js
+++ dadi/main.js
@@ -1,1 +1,14 @@
 // "Gioco dei dadi" PC vs. Human: chiedi un numero all'utente tra 1 e 6 (giocata human) e estrai un numero casuale tra 1 e 6 (giocata PC). Chi fa il numero più alto vince!
+
+// ask player for a number between 1 and 6
+var playerNumber = prompt("Please enter a number between 1 and 6");
+
+var cpuNumber = ( ( Math.floor( Math.random() * 10 ) % 6 ) + 1 );
+
+if ( playerNumber > cpuNumber ) {
+    document.writeln("Player: " + playerNumber + ", Cpu: " + cpuNumber + ". You win!");
+} else if ( playerNumber < cpuNumber ) {
+    document.writeln("Player: " + playerNumber + ", Cpu: " + cpuNumber + ". You lose!");
+} else {
+    document.writeln("Player: " + playerNumber + ", Cpu: " + cpuNumber + ". It's a tie!");
+}
